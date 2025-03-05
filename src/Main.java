74a8050d7589e33aa0ef99c7d815f5edd6304736
Titov Ilya <titov1608@gmail.com>
2025-03-05 09:13:59 +0300

    дз цыклы 2

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,90 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 1");
-        byte age = 24;
-        if (age >= 18) {
-            System.out.println("Он совершеннолетний");
-        }else {
-            System.out.println("Он не достиг совершеннолетия");
+        //TASK1
+        System.out.println("\nTASK1\n");
+        int saving = 0;
+        int month = 0;
+        int salary = 15_000;
+        while (saving <= 2_459_000) {
+            month++;
+            saving = saving + salary;
+            System.out.println("Месяц " + month + " сумма накоплений равна " + saving + " рублей");
+        }
+        //TASK2
+        System.out.println("\nTASK2\n");
+        int n = 0;
+        while (n < 10) {
+            System.out.print(++n + " ");
+            ;
+        }
+        System.out.println();
+        for (; n != 0; n--) {
+            System.out.print(n + " ");
+        }
+        //TASK3
+        System.out.println("\nTASK3\n");
+        int countryY = 12_000_000;
+        byte birth = 17;
+        byte death = 8;
+        byte year = 0;
+
+        while (year != 10) {
+            year++;
+            int loc = countryY / 1000;
+            int birthYear = birth * loc;
+            int deathYear = death * loc;
+            countryY -= deathYear;
+            countryY += birthYear;
+            System.out.println("Год " + year + ", численость населения состовляет " + countryY);
+        }
+        //TASK4 AND TASK5
+        System.out.println("\nTASK4 and TASK5\n");
+        int deposit = 15_000;
+        int percentageMonth = 7;
+        int totalSum = 12_000_000;
+        int monthDeposit = 0;
+
+        while (deposit <= totalSum) {
+            monthDeposit++;
+            deposit += (deposit / 100) * percentageMonth;
+            if (monthDeposit % 6 == 0) {
+                System.out.println("Месяц " + monthDeposit + " накопил " + deposit);
+            }
         }
 
-        System.out.println("задача 2");
-        int temperature = 7;
-        if (temperature < 5) {
-            System.out.println("На улице холодно, нужно надеть шапку");
-        }else if (temperature >= 5) {
-            System.out.println("Сегодня тепло, можно идти без шапки");
+        //TASK6
+        System.out.println("\nTASK6\n");
+        int saveYear = 9;
+        int depositVasiliy = 15_000;
+        int monthTotal = 0;
+
+        while (monthTotal != (saveYear*12)) {
+            monthTotal++;
+            depositVasiliy += (depositVasiliy / 100) * percentageMonth;
+            if (monthTotal % 6 == 0) {
+                System.out.println("Полугодие: " + monthTotal + " Сумма:" + depositVasiliy);
+            }
+        }
+        //TASK7
+        System.out.println("\nTASK7\n");
+        int firstFriday = 5;
+        int totalDayInMonth = 31;
+        for (; firstFriday< totalDayInMonth; firstFriday+=7) {
+            System.out.println("Сегодня пятница, " + firstFriday + "-е" + " число. Необходимо подготовить отчет");
         }
 
-        System.out.println("Задача 3");
-        int speed = 70;
-        if (speed > 60) {
-            System.out.println("Если скорость " + speed + " км/ч, то придется заплатить штраф.");
-        } else {
-            System.out.println("Если скорость " + speed + " км/ч, то можно ездить спокойно.");
+        //TASK8
+        System.out.println("\nTASK8\n");
+        short startYear = 2025-200;
+        short endYear = 2025+100;
+
+        while (startYear<=endYear) {
+            startYear++;
+            if (startYear % 79 == 0) {
+                System.out.println(startYear);
+            }
         }
 
-        System.out.println("Задача 4");
 
-        if (age >= 2 && age <= 6) {
-            System.out.println("Ему нужно ходить в детский сад.");
-        }else if (age >= 7 && age <= 17) {
-            System.out.println("Ему нужно ходить в школу");
-        }else if (age >= 18 && age <= 23) {
-            System.out.println("его место в университете.");
-        }else if (age >= 24)
-            System.out.println("пора ходить на работу.");
-
-        System.out.println("Задача 5");
-        int babyAge = 5;
-        if (babyAge < 5) {
-            System.out.println("Если возраст ребенка равен " + babyAge + " то он не может кататься на аттракционе.");
-        }else if (babyAge >= 5 && babyAge < 14) {
-            System.out.println("Если возраст ребенка равен " + babyAge + " то он может кататься только в сопровождении взрослого");
-        }else {
-            System.out.println("Если возраст ребенка равен " + babyAge + " то он может кататься без сопровождения взрослого.");
-        }
-
-        System.out.println("Задача 6");
-        int seats = 65;
-        if (seats <= 60 ) {
-            System.out.println("Есть стоячие и седячее места");
-        }else if (seats > 60 && seats <= 102) {
-            System.out.println("Свободны только стоячее");
-        }else {
-            System.out.println("Вагон полностью забит");
-        }
-        System.out.println("Задача 7");
-        int one = 22;
-        int two = 50;
-        int three = 32;
-        int max;
-        if (one >= two && one >= three) {
-            max = one;
-        } else if (two >= one && two >= three) {
-            max = two;
-        } else {
-            max = three;
-        }
-
-        System.out.println("Наибольшее число: " + max);
     }
 }
