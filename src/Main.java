8c95bba67b07950fa628057f7954f5ca44a7fb73
Titov Ilya <titov1608@gmail.com>
2025-03-26 15:50:40 +0300

    hm-string

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,19 @@ public class Main {
     public static void main(String[] args) {
         //TASK1
         System.out.println("\nTASK1\n");
-        int[] salary = {20, 30, 15, 21, 89};
-        int totalSalary = 0;
-        for (int i = 0; i < salary.length; i++) {
-            totalSalary += salary[i];
-        }
-        System.out.println("Сумма трат за месяц составила " + totalSalary + " рублей");
+        String firstName = " Ivan";
+        String midelName = "Ivanov ";
+        String lastName = " Ivanovich";
+        String fullname = midelName + " " + firstName + " " + lastName;
+        System.out.println("Ф.И.О. сотрудника - " + fullname);
 
         //TASK2
         System.out.println("\nTASK2\n");
-        int minSalary = salary[0];
-        int maxSalary = salary[0];
-        for (int salarys : salary) {
-            if (minSalary > salarys) {
-                minSalary = salarys;
-            }
-            if (maxSalary < salarys) {
-                maxSalary = salarys;
-            }
-        }
-        System.out.println("Минимальная сумма трат за неделю составила " + minSalary + " рублей");
-        System.out.println("Максимальная сумма трат за неделю составила " + maxSalary + " рублей");
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullname.toUpperCase());
 
-        //TASK3
+        //TASK2
         System.out.println("\nTASK3\n");
-        int totalSalarys = 0;
-        for (int s : salary) {
-            totalSalarys += s;
-        }
-        float age = (float) totalSalarys / salary.length;
-        System.out.println("Средняя сумма трат за месяц составила " + age + " рублей");
-
-        //TASK4
-        System.out.println("\nTASK4\n");
-        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
-        }
+        fullname = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullname.replace('ё', 'е'));
     }
 }
