fbbb5071c317c835cf77a85c338444a9aa7a42a6
Titov Ilya <titov1608@gmail.com>
2025-04-26 11:25:43 +0300

    hm-methods-object(fixed)

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String title;
     private Author author;
@@ -28,5 +30,17 @@ public class Book {
     @Override
     public String toString() {
         return title + " " + "author:" + author + " " + "publisher: " + publisher;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publisher == book.publisher && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publisher);
     }
 }
